7829a8dc364203dc60420e6310c2b83d765eb3d3
William Y Wong <wwong1987@gmail.com>
2019-04-15 16:47:32 -0700

    working on deploying and resolving CORs

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL
+  DB_URL: process.env.DATABASE_URL
     || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
   TEST_DB_URL: process.env.TEST_DB_URL || "postgresql://dunder-mifflin@localhost/spaced-repetition-test",
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
